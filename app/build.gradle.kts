8238543ab052b486d79adbf07611085b4943385f
MrBoombastic <contact@amroz.xyz>
2026-02-12 22:20:12 +0100

    chore: update versionCode to 15 and versionName to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ configure<ApplicationExtension> {
         applicationId = "com.mrboombastic.buwudzik"
         minSdk = 34
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.5.2"
+        versionCode = 15
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
